51c2507386b57b17d12b8543abf367f1a76312ff
Santiago Barletta <santiagobarletta96@gmail.com>
2022-09-08 20:34:45 -0300

    Create SideBar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
-import { Box, Stack, Typography } from "@mui/material"
-import { useEffect, useState } from "react"
+import { Box, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Sidebar } from ".";
 
 
 
@@ -9,10 +10,10 @@ export const Feed = () => {
 
       <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
 
-        {/* <Sidebar/> */}
+        <Sidebar/> 
 
-        <Typography>
-
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
+          Copyright 2022 Santiago Barletta
         </Typography>
       
       </Box>
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {Feed} from "./Feed";
 export {Navbar}  from "./Navbar";
 export {SearchFeed}  from "./SearchFeed";
 export {VideoDetail}  from "./VideoDetail";
+export {Sidebar}  from "./Sidebar";
